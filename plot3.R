53aa1f133de1ca9ef4d5503eb93b97777718df83
aksh1315 <aksh1315@gmail.com>
2020-08-08 01:16:16 +0530

    Week 1

---
+++ plot3.R
@@ -0,0 +1,13 @@
+data=read.table("household_power_consumption.txt",header = TRUE,sep = ";",na.strings = "?")
+data$Date=as.Date(data$Date,format = "%d/%m/%Y")
+data=subset(data,subset = (Date>="2007-02-01" & Date<= "2007-02-02"))
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+data$datetime=as.POSIXct(data$datatime)
+within(data,datetime)
+
+with(data,plot(datetime,Sub_metering_1 , type = "l", ylab = "Energy sub metering", xlab = ""))
+with(data,lines(datetime,Sub_metering_2 , col = "Red"))
+with(data,lines(datetime,Sub_metering_3 , col = "Blue"))
+legend("topright", lty = 1, col = c("black", "red", "blue"),legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.copy(png,file="plot3.png",height=480,width=480)
+dev.off()         
